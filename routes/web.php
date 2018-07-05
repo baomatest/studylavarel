1ea1e09c9b7871d06dd19caa394828bef355b431
baomatest <vimojnguoi1@gmail.com>
2018-07-05 17:32:54 +0700

    first commit1

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+/*Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home','HomeController@index')->name('home');
+        // User đăng nhập mới vào được backend
+
+Auth::routes();
+
+  /*  Route::get('user/profile', function () {
+        // User đăng nhập mới vào được trang cá nhân
+    });*
+});*/
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/abc','menuinsertcontroller@index');
